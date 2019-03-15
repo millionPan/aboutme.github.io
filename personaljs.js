835d78c331ff5841ffe086534d7320e3b89ac8d6
millionPan <809251311@qq.com>
2019-03-15 11:09:45 +0800

    version2.0:将所有canvas改成svg

--- personaljs.js
+++ personaljs.js
@@ -1,70 +1,23 @@
-//一排圆形
-function drawcirclelist(x,y,n,ctx,color){
-    ctx.fillStyle =color;
-    ctx.beginPath();
-   for(var i=0;i<n;i++){   
-       ctx.arc(x+x*2*i,y,x,0,2*Math.PI);                   
-    }
-    ctx.fill();
-    ctx.closePath();
-}
-//每个小栏目的分割条
-function sepi(ctx,y,r,color,text){
-    ctx.fillStyle =color;
-    ctx.beginPath()
-    ctx.arc(120,y,r,Math.PI/6,Math.PI*5/6,true);
-    ctx.fillRect(93,y+r/2,766,3);
-    ctx.closePath();
-    ctx.fill();
-    ctx.beginPath();
-    ctx.lineCap='round';
-    ctx.strokeStyle=color;
-    ctx.moveTo(830, y);
-    ctx.lineTo(850, y);
-    ctx.lineWidth=18;
-    ctx.stroke();
-    ctx.closePath();
-    ctx.font="bold 25px '微软雅黑'";
-    ctx.fillText(text,170,45);
-    }
-    //箭头
-    function arrow(direction,y,ctx){
-        for (var i = 0; i < 2; i++){
-        ctx.strokeStyle="white";
-        ctx.beginPath();
-        ctx.lineWidth=3;
-        ctx.lineJoin = "round";
-        if(direction=="down"){
-            ctx.moveTo(110, y+8*i);
-            ctx.lineTo(120,y+8+8*i);
-            ctx.lineTo(130,y+8*i);
-       }else{
-           ctx.moveTo(110, y+8*(i+1));
-           ctx.lineTo(120,y-8+8*(i+1));
-           ctx.lineTo(130,y+8*(i+1));
-       };
-        ctx.stroke();}
-        }
+
+for(var i= 0;i<64;i++){
+    var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    c.setAttribute('r', 7.5);//随机半径
+    c.setAttribute('cx', 7.5+7.5*2*i);//随机圆心 X坐标
+    c.setAttribute('cy', 230);//随机圆心 Y坐标
+    c.setAttribute('fill', 'rgb(1,97,105)');//填充色
     
-    // 箭头滑动动画
-        function act(){
-            var y=28;
-            var speedx = 1/2;
-            //清除画布
-            ctx.clearRect(0,0,960,60);
-            sepi(ctx,40,32,"rgb(1,97,105)","教育背景");
-            //更新位置
-            y +=speedx;
-            
-            // 重绘
-            arrow(direction,y,ctx);
-            //当x位置小于等于0或大于等于canvas画布的宽度减去方块的宽度
-            if(y<=20 ||y>32){
-                speedx *=-1;
-            }
-            window.requestAnimationFrame(act);
-        }
+    headerbgi.appendChild(c);
    
+}
+for(var i= 0;i<64;i++){
+    var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    c.setAttribute('r', 7.5);
+    c.setAttribute('cx', 7.5+7.5*2*i);
+    c.setAttribute('cy', 7.5);
+    c.setAttribute('fill', 'rgb(1,97,105)');
+    footerbgi.appendChild(c);
+                
+};  
     // 点击显示隐藏echarts
     function showecharts(plid){
         var echartbox=document.getElementById(plid);
@@ -76,24 +29,38 @@ function sepi(ctx,y,r,color,text){
        
             echartbox.style.display="none";
         }
+      
     };
 
 
-    function changeDir(plid,text){
-        var canvas = document.getElementById(plid);
-        var ctx=canvas.getContext('2d');
-        if(direction=="up"){
-             direction="down"; 
-             ctx.clearRect(0,0,960,60);
-             sepi(ctx,40,32,"rgb(1,97,105)",text);
-             arrow(direction,28,ctx);
+    function changeDire(){
+        var arrowde=document.getElementById("arrowdowne");
+        var arrowue=document.getElementById("arrowupe");
+        
+        if(arrowde.style.display=="none"){
            
-          }else{
-             direction="up" ;
-             ctx.clearRect(0,0,960,60);
-             sepi(ctx,40,32,"rgb(1,97,105)",text);
-             arrow(direction,28,ctx);
-             
-          }   
- 
-}
+            arrowde.style.display="block";
+            arrowue.style.display="none";
+        }else{
+       
+            arrowde.style.display="none";
+            arrowue.style.display="block";
+        }
+          } 
+         
+
+function changeDirs(){
+    var arrowde=document.getElementById("arrowdowns");
+    var arrowue=document.getElementById("arrowups");
+    
+    if(arrowde.style.display=="none"){
+       
+        arrowde.style.display="block";
+        arrowue.style.display="none";
+    }else{
+   
+        arrowde.style.display="none";
+        arrowue.style.display="block";
+    }  
+      } 
+        
